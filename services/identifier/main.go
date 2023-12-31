badfbc788ede7aa30b56b01fdf054f54e01d9ed8
kdotwei <kwei.mg09@nycu.edu.tw>
2023-12-31 14:05:39 +0800

    update the file structure

---
+++ services/identifier/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	username string
+	password string
+)
+
+func LoginPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "login.html", nil)
+}
+
+func LoginAuth(c *gin.Context) {
+	var (
+		username string
+		password string
+	)
+	if in, isExist := c.GetPostForm("username"); isExist && in != "" {
+		username = in
+	} else {
+		c.HTML(http.StatusBadRequest, "login.html", gin.H{
+			"error": errors.New("Empty username"),
+		})
+		return
+	}
+	if in, isExist := c.GetPostForm("password"); isExist && in != "" {
+		password = in
+	} else {
+		c.HTML(http.StatusBadRequest, "login.html", gin.H{
+			"error": errors.New("Empty password"),
+		})
+		return
+	}
+	if err := Auth(username, password); err == nil {
+		c.HTML(http.StatusOK, "login.html", gin.H{
+			"success": "Login Success",
+		})
+		return
+	} else {
+		c.HTML(http.StatusUnauthorized, "login.html", gin.H{
+			"error": err,
+		})
+		return
+	}
+}
+
+func main() {
+	server := gin.Default()
+	server.LoadHTMLGlob("template/html/*")
+
+	// Read the static files
+	server.Static("/assets", "./template/assets")
+	server.GET("/login", LoginPage)
+	server.POST("/login", LoginAuth)
+	server.Run(":8888")
+}
