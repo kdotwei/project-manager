cc29717ecc4f6f82d7b0a7025ab60100c372a500
kdotwei <kwei.mg09@nycu.edu.tw>
2024-01-02 00:36:50 +0800

    add user-manager container | add nginx protected path

--- services/identifier/middleware/jwt.go
+++ services/identifier/middleware/jwt.go
@@ -1,10 +1,8 @@
 package middleware
 
 import (
-	"net/http"
 	"time"
 
-	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 )
 
@@ -44,30 +42,4 @@ func VerifyToken(tokenString string) (*Claims, bool) {
 	}
 
 	return claims, token.Valid
-}
-
-// IsLoggedIn checks if the user is logged in by validating the JWT token
-func IsLoggedIn() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		tokenString, err := c.Cookie("token") // Assuming token is stored in a cookie
-		if err != nil {
-			// Token not found in cookie, redirect to login
-			c.Redirect(http.StatusTemporaryRedirect, "/identifier/login")
-			c.Abort()
-			return
-		}
-
-		// Verify the token
-		claims, valid := VerifyToken(tokenString)
-		if !valid {
-			// Token is invalid, redirect to login
-			c.Redirect(http.StatusTemporaryRedirect, "/identifier/login")
-			c.Abort()
-			return
-		}
-
-		// Token is valid, set the username in context and continue
-		c.Set("username", claims.Username)
-		c.Next()
-	}
 }
